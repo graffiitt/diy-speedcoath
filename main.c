626ced85f88d37d8a0e91e7044e69db5262fa7a6
vlad mac <vladikkopylov31@gmail.com>
2024-07-14 12:50:06 +0300

    [ADD] init repo

---
+++ main.c
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2023 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "pico/stdlib.h"
+ 
+
+int main()
+{
+    stdio_init_all();
+ 
+    
+    while (1)
+    {
+    }
+
+    return 0;
+}
